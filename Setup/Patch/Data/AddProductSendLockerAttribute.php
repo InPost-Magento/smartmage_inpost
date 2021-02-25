e0335da7284b0fe1c97387070942cde86b772894
Rafał Świtala <rafal.switala@smartmage.pl>
2021-02-25 20:07:24 +0100

    INPOST-4 add data patches with product attributes

---
+++ Setup/Patch/Data/AddProductSendLockerAttribute.php
@@ -0,0 +1,108 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Smartmage\Inpost\Setup\Patch\Data;
+
+use Magento\Catalog\Model\Product;
+use Magento\Eav\Model\Entity\Attribute\Source\Boolean;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Framework\Setup\Patch\PatchRevertableInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface;
+
+class AddProductSendLockerAttribute implements DataPatchInterface, PatchRevertableInterface
+{
+
+    const SEND_LOCKER_ATTRIBUTE_NAME = 'send_with_locker';
+
+    /**
+     * @var ModuleDataSetupInterface
+     */
+    protected ModuleDataSetupInterface $moduleDataSetup;
+
+    /**
+     * @var EavSetupFactory
+     */
+    protected EavSetupFactory $eavSetupFactory;
+
+    /**
+     * AddProductSendLockerAttribute constructor.
+     * @param ModuleDataSetupInterface $moduleDataSetup
+     * @param EavSetupFactory $eavSetupFactory
+     */
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup,
+        EavSetupFactory $eavSetupFactory
+    ) {
+        $this->moduleDataSetup = $moduleDataSetup;
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function apply(): AddProductSendLockerAttribute
+    {
+        $this->moduleDataSetup->getConnection()->startSetup();
+
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $this->moduleDataSetup]);
+        $eavSetup->addAttribute(
+            Product::ENTITY,
+            self::SEND_LOCKER_ATTRIBUTE_NAME,
+            [
+                'type' => 'int',
+                'label' => 'Send with Locker',
+                'input' => 'select',
+                'user_defined' => false,
+                'global' => ScopedAttributeInterface::SCOPE_STORE,
+                'group' => 'General',
+                'source' => Boolean::class,
+                'sort_order' => 1000,
+                'visible_on_front' => false,
+                'used_in_product_listing' => false,
+                'searchable' => false,
+                'filterable' => false,
+                'comparable' => false,
+            ]
+        );
+
+        $this->moduleDataSetup->getConnection()->endSetup();
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getDependencies(): array
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function revert()
+    {
+        $this->moduleDataSetup->getConnection()->startSetup();
+
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $this->moduleDataSetup]);
+        $eavSetup->removeAttribute(
+            Product::ENTITY,
+            self::SEND_LOCKER_ATTRIBUTE_NAME
+        );
+
+        $this->moduleDataSetup->getConnection()->endSetup();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getAliases(): array
+    {
+        return [];
+    }
+
+}
