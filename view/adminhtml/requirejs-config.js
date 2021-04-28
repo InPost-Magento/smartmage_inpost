3ca6ad00c5c49a19703b6282945d3419df1ae727
Robert Biernat <robert@smartmage.pl>
2021-04-28 14:18:48 +0200

    [INPOST-53] Fixed inpost shipment grid listing checkboxes state after print label mass action.

--- view/adminhtml/requirejs-config.js
+++ view/adminhtml/requirejs-config.js
@@ -10,7 +10,13 @@ var config = {
             'https://geowidget.easypack24.net/js/sdk-for-javascript'
         ],
     },
-
+    config: {
+        mixins: {
+            'Magento_Ui/js/grid/massactions': {
+                'Smartmage_Inpost/js/grid/massactions-mixin': true
+            },
+        }
+    },
     shim: {
         'easyPackWidget': ['jquery', 'inPostSdk'],
         'inPostShowModal': ['jquery', 'inPostSdk'],
