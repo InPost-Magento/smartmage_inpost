2019b24d3e04503b6332a2c928845a39d375281d
Rafał Świtala <rafal.switala@smartmage.pl>
2021-07-13 13:11:43 +0200

    HEL-275

--- Model/ApiShipx/Service/Shipment/AbstractCreate.php
+++ Model/ApiShipx/Service/Shipment/AbstractCreate.php
@@ -140,7 +140,7 @@ abstract class AbstractCreate extends AbstractService
         $this->requestBody['service'] = $this->shippingMethods::INPOST_MAPPER[$data['service']];
         $this->requestBody['reference'] = $data['reference'];
         $this->requestBody['only_choice_of_offer'] = false;
-        $this->requestBody['external_customer_id'] = 'magento';
+        $this->requestBody['external_customer_id'] = 'Magento';
 
         if ($this->configProvider->getChangeAddress()) {
             $this->requestBody['sender'] = [
