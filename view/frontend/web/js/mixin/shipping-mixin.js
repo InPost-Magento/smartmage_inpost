e5a4a375e53a88360f0bfb7bbab85c8888112a36
Paweł Koteluk <pawel.koteluk@smartmage.pl>
2021-12-23 13:40:12 +0100

    fix js validate selected point

--- view/frontend/web/js/mixin/shipping-mixin.js
+++ view/frontend/web/js/mixin/shipping-mixin.js
@@ -31,37 +31,34 @@ define(
                 },
 
                 validateShippingInformation: function() {
-                    var superResult = this._super();
                     var self = this;
 
-                    if(superResult) {
-                        if(quote.shippingMethod()) {
+                    if(quote.shippingMethod()) {
+                        var ShippingMethodCode = quote.shippingMethod().method_code+'_'+quote.shippingMethod().carrier_code;
 
-                            var ShippingMethodCode = quote.shippingMethod().method_code+'_'+quote.shippingMethod().carrier_code;
+                        if (ShippingMethodCode === 'standard_inpostlocker' || ShippingMethodCode === 'standardcod_inpostlocker' || ShippingMethodCode === 'standardeow_inpostlocker' || ShippingMethodCode === 'standardeowcod_inpostlocker' ) {
+                            var pointDataDB = checkoutData.getShippingInPostPoint();
 
-                            if (ShippingMethodCode === 'standard_inpostlocker' || ShippingMethodCode === 'standardcod_inpostlocker' || ShippingMethodCode === 'standardeow_inpostlocker' || ShippingMethodCode === 'standardeowcod_inpostlocker' ) {
-                                var pointDataDB = checkoutData.getShippingInPostPoint();
+                            if( typeof pointDataDB === 'undefined' || pointDataDB === null || pointDataDB.name.length === 0){
+                                self.errorValidationMessage(
+                                    $t('Please select a pickup point')
+                                );
+                                return false;
 
-                                if( typeof pointDataDB === 'undefined' || pointDataDB === null || pointDataDB.name.length === 0){
-                                    self.errorValidationMessage(
-                                        $t('Please select a pickup point')
-                                    );
-                                    return false;
-
-                                } else {
-                                    if(ShippingMethodCode === 'standardcod_inpostlocker' || ShippingMethodCode === 'standardeowcod_inpostlocker') {
-                                        if(!pointDataDB.type.includes('parcel_locker')) {
-                                            self.errorValidationMessage(
-                                                $t('The selected point does not support the cash on delivery method')
-                                            );
-                                            return false;
-                                        }
+                            } else {
+                                if(ShippingMethodCode === 'standardcod_inpostlocker' || ShippingMethodCode === 'standardeowcod_inpostlocker') {
+                                    if(!pointDataDB.type.includes('parcel_locker')) {
+                                        self.errorValidationMessage(
+                                            $t('The selected point does not support the cash on delivery method')
+                                        );
+                                        return false;
                                     }
                                 }
                             }
                         }
-                        return true;
                     }
+
+                    return this._super();
                 },
             });
         }
