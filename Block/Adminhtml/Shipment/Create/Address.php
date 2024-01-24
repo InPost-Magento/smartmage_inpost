be8d611a1c5ace6b0367b3b4022dd97d611b0740
KamilBorkowski <poczta@kamilborkowski.pl>
2024-01-24 14:52:24 +0100

    Fix for missed files

---
+++ Block/Adminhtml/Shipment/Create/Address.php
@@ -0,0 +1,9 @@
+<?php
+namespace Smartmage\Inpost\Block\Adminhtml\Shipment\Create;
+
+use Magento\Backend\Block\Template;
+
+class Address extends Template
+{
+    protected $_template = 'Smartmage_Inpost::shipment/create/address.phtml';
+}
