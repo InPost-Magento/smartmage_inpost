c925edbca423d100950aff71da6ab120c508c689
KamilBorkowski <poczta@kamilborkowski.pl>
2025-08-23 15:04:08 +0200

    Setup patch refactor

--- Setup/Patch/Data/AddProductAlcoholAttribute.php
+++ Setup/Patch/Data/AddProductAlcoholAttribute.php
@@ -4,105 +4,26 @@ declare(strict_types=1);
 
 namespace Smartmage\Inpost\Setup\Patch\Data;
 
-use Magento\Catalog\Model\Product;
 use Magento\Eav\Model\Entity\Attribute\Source\Boolean;
-use Magento\Framework\Setup\Patch\DataPatchInterface;
-use Magento\Framework\Setup\Patch\PatchRevertableInterface;
-use Magento\Framework\Setup\ModuleDataSetupInterface;
-use Magento\Eav\Setup\EavSetupFactory;
-use Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface;
 
-class AddProductAlcoholAttribute implements DataPatchInterface, PatchRevertableInterface
+class AddProductAlcoholAttribute extends AbstractProductAttributePatch
 {
+    protected const GROUP_NAME = 'General';
+    protected const SORT_ORDER = 32769;
 
-    public const INPOST_ALCOHOL = 'inpost_alcohol';
-
-    /**
-     * @var ModuleDataSetupInterface
-     */
-    protected $moduleDataSetup;
-
-    /**
-     * @var EavSetupFactory
-     */
-    protected $eavSetupFactory;
-
-    /**
-     * AddProductSendLockerAttribute constructor.
-     * @param ModuleDataSetupInterface $moduleDataSetup
-     * @param EavSetupFactory $eavSetupFactory
-     */
-    public function __construct(
-        ModuleDataSetupInterface $moduleDataSetup,
-        EavSetupFactory $eavSetupFactory
-    ) {
-        $this->moduleDataSetup = $moduleDataSetup;
-        $this->eavSetupFactory = $eavSetupFactory;
+    protected static function attributeCode(): string
+    {
+        return 'inpost_alcohol';
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function apply(): AddProductAlcoholAttribute
+    protected function attributeDefinition(): array
     {
-        $this->moduleDataSetup->getConnection()->startSetup();
-
-        $eavSetup = $this->eavSetupFactory->create(['setup' => $this->moduleDataSetup]);
-        $eavSetup->addAttribute(
-            Product::ENTITY,
-            self::INPOST_ALCOHOL,
-            [
-                'type' => 'int',
-                'label' => 'Send with InPost SmartCourier',
-                'input' => 'boolean',
-                'user_defined' => false,
-                'global' => ScopedAttributeInterface::SCOPE_STORE,
-                'group' => 'General',
-                'source' => Boolean::class,
-                'default' => 0,
-                'sort_order' => 32769,
-                'visible_on_front' => false,
-                'used_in_product_listing' => false,
-                'searchable' => false,
-                'filterable' => false,
-                'comparable' => false,
-            ]
-        );
-
-        $this->moduleDataSetup->getConnection()->endSetup();
-
-        return $this;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public static function getDependencies(): array
-    {
-        return [];
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function revert()
-    {
-        $this->moduleDataSetup->getConnection()->startSetup();
-
-        $eavSetup = $this->eavSetupFactory->create(['setup' => $this->moduleDataSetup]);
-        $eavSetup->removeAttribute(
-            Product::ENTITY,
-            self::INPOST_ALCOHOL
-        );
-
-        $this->moduleDataSetup->getConnection()->endSetup();
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function getAliases(): array
-    {
-        return [];
+        return [
+            'type'   => 'int',
+            'label' => 'Send with InPost SmartCourier',
+            'input'  => 'boolean',
+            'source' => Boolean::class,
+            'default'=> 0,
+        ];
     }
 }
